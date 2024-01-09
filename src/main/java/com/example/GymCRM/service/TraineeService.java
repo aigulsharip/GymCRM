c3878d6eec41b1320b0e2295ea600eb8e2d5ff41
aigulsharip <aigul.sharip@nu.edu.kz>
2024-01-09 14:54:20 +0600

    Modification to pass user_id while creating Trainee

--- src/main/java/com/example/GymCRM/service/TraineeService.java
+++ src/main/java/com/example/GymCRM/service/TraineeService.java
@@ -1,7 +1,9 @@
 package com.example.GymCRM.service;
 
 import com.example.GymCRM.entity.Trainee;
+import com.example.GymCRM.entity.User;
 import com.example.GymCRM.repository.TraineeRepository;
+import com.example.GymCRM.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,17 +13,21 @@ import java.util.List;
 public class TraineeService {
 
     private final TraineeRepository traineeRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public TraineeService(TraineeRepository traineeRepository) {
+    public TraineeService(TraineeRepository traineeRepository, UserRepository userRepository) {
         this.traineeRepository = traineeRepository;
+        this.userRepository = userRepository;
     }
 
-    // Create operation
     public Trainee createTrainee(Trainee trainee) {
+        User existingUser = userRepository.findById(trainee.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found with ID: " + trainee.getUserId()));
+
+        trainee.setUserId(existingUser.getId());
         return traineeRepository.save(trainee);
     }
-
     // Read operations
     public List<Trainee> getAllTrainees() {
         return traineeRepository.findAll();
